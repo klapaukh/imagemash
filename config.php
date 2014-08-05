ed1b35c9e8ecbe7230a029d146b5829f9f1d8117
Roman Klapaukh <roma@ecs.vuw.ac.nz>
2014-08-06 08:58:10 +1200

    Change adjective

--- config.php
+++ config.php
@@ -6,7 +6,7 @@
 $nouns = "images";
 
 # What is the adjective you're asking about?
-$adjective = "more symmetric";
+$adjective = "more symmetrical";
 
 # How many pairs of images do you want to ask people about?
 $pairs = 10;
